e4a83a1a7bb6d130d177b53f30139dbd8be75791
Petar Obradovic <perazmn@gmail.com>
2024-12-26 19:04:36 +0100

    Feat add human interaction

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,9 @@ const r = @cImport({
     @cInclude("rlgl.h");
 });
 
+const RED = 2;
+const YELLOW = 1;
+
 const radius: c_int = 50;
 const columnSize: c_int = radius * 2;
 
@@ -12,16 +15,14 @@ const rows = 6;
 const columns = 7;
 
 pub fn main() !void {
+    var isYellow = true;
+
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
     r.SetConfigFlags(r.FLAG_VSYNC_HINT | r.FLAG_MSAA_4X_HINT | r.FLAG_WINDOW_RESIZABLE);
     r.InitWindow(700, 700, "test");
 
     var gameTable: [rows][columns]u2 = std.mem.zeroes([rows][columns]u2);
-    gameTable[0][0] = 1;
-    gameTable[0][1] = 1;
-    gameTable[0][2] = 1;
-    gameTable[1][6] = 2;
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
@@ -42,8 +43,15 @@ pub fn main() !void {
         const windowWidth = r.GetRenderWidth();
         _ = windowWidth; // autofix
         const windowHeight = r.GetRenderHeight();
+        if (r.IsMouseButtonPressed(r.MOUSE_BUTTON_LEFT)) {
+            const mousePos = r.GetMousePosition();
+            _ = mousePos; // autofix
+            placePoint(&isYellow, &gameTable, r.GetMousePosition());
+        }
 
+        // DRAW
         r.BeginDrawing();
+        defer r.EndDrawing();
         r.ClearBackground(r.WHITE);
         for (1..columns) |i| {
             const x = @as(c_int, @intCast(i)) * columnSize;
@@ -58,13 +66,29 @@ pub fn main() !void {
                         @as(c_int, @intCast(j + 1)) * columnSize - radius,
                         windowHeight - columnSize * @as(c_int, @intCast(i)) - radius,
                         radius,
-                        if (value == 1) r.RED else r.YELLOW,
+                        if (value == YELLOW) r.YELLOW else r.RED,
                     );
                 }
             }
         }
-
-        defer r.EndDrawing();
+    }
+}
+fn placePoint(isYellow: *bool, gameTable: *[rows][columns]u2, mousePosition: r.struct_Vector2) void {
+    const column: usize = @intFromFloat(@divFloor(mousePosition.x, @as(f32, @floatFromInt(columnSize))));
+    if (column > columns - 1) {
+        std.log.info("Clicked outside", .{});
+        return;
+    }
+    if (gameTable.*[rows - 1][column] != 0) {
+        std.log.info("Column full", .{});
+        return;
+    }
+    for (gameTable, 0..) |row, i| {
+        if (row[column] == 0) {
+            gameTable[i][column] = if (isYellow.*) 1 else 2;
+            isYellow.* = !isYellow.*;
+            return;
+        }
     }
 }
 
